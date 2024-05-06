2cdc61352d3609919e8f5623189403b00f59c818
Kim Dong Hyun <98632435+DDonghyeo@users.noreply.github.com>
2024-05-07 00:47:34 +0900

    4주차 : Spring Security

---
+++ src/main/java/com/likelion/lionshop_sample/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.likelion.lionshop_sample.controller;
+
+import com.likelion.lionshop_sample.dto.response.JwtDto;
+import com.likelion.lionshop_sample.service.AuthService;
+import com.likelion.lionshop_sample.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    //토큰 재발급 API
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(@RequestBody JwtDto jwtDto) {
+
+        log.info("[ Auth Controller ] 토큰을 재발급합니다. ");
+
+        return ResponseEntity.ok(authService.reissueToken(jwtDto));
+    }
+}
